2e429bd45bf8c0ef3339259918c4076b36669b57
Ваше имя <alesya-13@mail.ru>
2020-09-18 21:49:24 +0300

    feat: add solusion task 5

---
+++ src/TeachMeSkills.DotNet.Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int a = 5;
+            int b = 10;
+            int s = a + b;
+            int v = b - a;
+            int u = a * b;
+            int del = b / a;
+            Console.WriteLine(s);
+            Console.WriteLine(v);
+            Console.WriteLine(u);
+            Console.WriteLine(del);
+            Console.ReadLine();
+
+        }
+    }
+}
