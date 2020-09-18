ffef6bf457cfee0c715393d7292320d6115d0b0b
Ваше имя <alesya-13@mail.ru>
2020-09-18 21:43:03 +0300

    feat: add solusion task 4

---
+++ src/TeachMeSkills.DotNet.Task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal h = 3M;
+            decimal r = 14M;
+            decimal pi = 3.141592653M;
+            var V = pi * r * h*2;
+            var S = 2 *pi * r * (r+h);
+            Console.WriteLine(V);
+            Console.WriteLine(S);
+            Console.ReadLine();
+
+        }
+    }
+}
