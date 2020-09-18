d96aba50ee3e3af43b1449184568e5cfc8fc3722
Ваше имя <alesya-13@mail.ru>
2020-09-18 21:32:15 +0300

    feat: add solusion task 3

---
+++ src/eachMeSkills.DotNet.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace eachMeSkills.DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.141592653M;
+            decimal r = 3.15M;
+            decimal a = pi*r*r;
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
