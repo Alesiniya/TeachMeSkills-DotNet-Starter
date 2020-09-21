18d00186b33c9c0505b82b66ab684bc95c04b2b3
Ваше имя <alesya-13@mail.ru>
2020-09-22 01:10:43 +0300

    feat: add the solution task 9

---
+++ src/TeachMeSkills.DotNet. Task 8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet._Task_8
+{
+    using System;
+
+    namespace TeachMeSkills.DotNet.Task6
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.Write("Enter your world: ");
+                string userInput = Console.ReadLine();
+                bool result = World.TryParse(userInput, out world);
+                if (result)
+                {
+                    Console.WriteLine(date.DayOfWeek);
+                }
+                else
+                {
+                    Console.WriteLine("incorrect input");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
---
+++ src/TeachMeSkills.DotNet.Task9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to check");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2==1)
+            {
+                Console.WriteLine("odd number");
+
+            }
+            else
+            {
+                Console.WriteLine("even number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
