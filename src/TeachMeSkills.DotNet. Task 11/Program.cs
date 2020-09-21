ded53c4e690760c059055ada7ebe9cc5f88d2314
Ваше имя <alesya-13@mail.ru>
2020-09-22 02:20:17 +0300

    feat: add the solution task 11

---
+++ src/TeachMeSkills.DotNet. Task 11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet._Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the degree to which you want to raise 2");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a <=32)
+            {
+                Console.WriteLine("You can raise 2 into this degree");
+            }
+            else if (a >= 33 || a < 0)
+            {
+                Console.WriteLine("you can not raise 2 into this degree");
+            }
+            Console.ReadKey();
+        }
+    }
+}
