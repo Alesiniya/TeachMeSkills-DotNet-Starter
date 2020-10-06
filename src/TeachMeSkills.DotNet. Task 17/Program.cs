ee929614fe0b29f02ddae3b56fc0a6b85fa92625
Ваше имя <alesya-13@mail.ru>
2020-10-06 03:13:26 +0300

    feat: add the solution task 17

--- src/TeachMeSkills.DotNet. Task 17/Program.cs
+++ src/TeachMeSkills.DotNet. Task 17/Program.cs
@@ -6,7 +6,14 @@ namespace TeachMeSkills.DotNet._Task_17
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the amount in dollars");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the rate to convert to euro");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = a * b;
+            Console.WriteLine("Your currency amount in euros is ");
+            Console.WriteLine(c);
+            Console.ReadKey();
         }
     }
 }
