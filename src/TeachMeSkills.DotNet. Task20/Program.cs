f7b45263d1842d7710d86a89c6bfad0557204749
Ваше имя <alesya-13@mail.ru>
2020-10-14 03:22:01 +0300

    feat: add the solution task 20

---
+++ src/TeachMeSkills.DotNet. Task20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet._Task20
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Calculate();
+            Console.ReadKey();
+        }
+
+        private static void Calculate()
+        {
+            Console.WriteLine("Enter your 1 number ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your 2 number ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your 3 number ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double s = (a + b + c) / 3;
+            Console.WriteLine("Arithmetic mean");
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ src/eachMeSkills.DotNet.Task19/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace eachMeSkills.DotNet.Task19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your 1 number ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your 2 number ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            bool s = false;
+            if (a < b)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    s = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            s = false;
+                            break;
+                        }
+                    }
+                    if (s)
+                        Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
