15c6f2eca66227b658a758e4af171730339aedd2
Ваше имя <alesya-13@mail.ru>
2020-11-03 02:10:09 +0300

    feat: add the solution task 23

---
+++ src/TeachMeSkills.DotNet.Task 23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet.Task_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indicate the number of deliveries");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", x, Factorial(x));
+            Console.ReadLine();
+        }
+
+        static int Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
