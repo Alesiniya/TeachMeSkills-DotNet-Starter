ff285d6f242ed82e7af656911e883cb7b5c9d9a9
Alesiniya <alesya-13@mail.ru>
2020-11-28 01:49:01 +0300

    feat: add the solution task 8

---
+++ src/TeachMeSkills.DotNet. Task 25/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills.DotNet._Task_25
+{
+    class Program
+    {
+        static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
+
+        // Change the array by reversing its elements.
+        static void ChangeArray(string[] arr) => Array.Reverse(arr);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- src/TeachMeSkills.DotNet. Task 8/Program.cs
+++ src/TeachMeSkills.DotNet. Task 8/Program.cs
@@ -9,11 +9,42 @@ namespace TeachMeSkills.DotNet._Task_8
         static void Main(string[] args)
         {
             Console.WriteLine("Write a word about the weather in Russian");
-            string word = Console.ReadLine();
-            if (word=="d") Console.Write("Your translation show");
-            else if (word=="hot") Console.Write("Your translation жарко");
-            else Console.Write(" ");
 
+            string world = Console.ReadLine().ToLower();
+
+            switch (world)
+            {
+                case "снег":
+                    Console.WriteLine("show");
+                    break;
+                case "жарко":
+                    Console.WriteLine("hot");
+                    break;
+                case "холодно":
+                    Console.WriteLine("cold");
+                    break;
+                case "солнце":
+                    Console.WriteLine("sun");
+                    break;
+                case "дождь":
+                    Console.WriteLine("rain");
+                    break;
+                case "облачно":
+                    Console.WriteLine("cloudy");
+                    break;
+                case "туман":
+                    Console.WriteLine("fog");
+                    break;
+                case "град":
+                    Console.WriteLine("hail");
+                    break;
+                case "давление":
+                    Console.WriteLine("pressure");
+                    break;
+                case "уровень опасности":
+                    Console.WriteLine("hazard level");
+                    break;
+            }
             Console.ReadKey();
         }
     }
